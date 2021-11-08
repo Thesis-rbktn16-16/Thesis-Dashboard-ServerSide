3a399676ce4a958d3880380f398ad07a3b5e9fb7
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-08 14:40:06 +0100

    auth done

--- bycycle/Admin/AdminController.js
+++ bycycle/Admin/AdminController.js
@@ -1,10 +1,11 @@
 var Auth = require("../Authentication/Auth");
 var Bycycle = require("../Bycycle");
-//
 exports.createAdmin = (req, res) => {
-  console.log(req.body);
-  let { User, Password, Email } = req.body;
-  Auth.HashPass(Password).then((Hashed) => {
+  console.log("---------------", req.body);
+
+  let { User, Password, Email } = req.body.RegisterAdmin;
+  Auth.HashPass(req.body.RegisterAdmin.Password).then((Hashed) => {
+    console.log(Hashed);
     Bycycle.AdminModel.create({ User, Password: Hashed, Email })
       .then((result) => {
         console.log("result", result);
@@ -17,15 +18,15 @@ exports.createAdmin = (req, res) => {
 };
 
 exports.CheckIfThePassRight = (req, res) => {
-  let { User, Password } = req.body;
+  let { User, Password } = req.body.LoginAdmin;
 
   Bycycle.AdminModel.findOne({ User: User }, (err, result) => {
     let hashedPass = result.Password;
     let UserNameFromDataBase = result.User;
 
     if (UserNameFromDataBase === User) {
-      Auth.comparePass(Password, hashedPass).then((bool) => {
-        if (bool) {
+      Auth.comparePass(Password, hashedPass).then((r) => {
+        if (r) {
           res.status(201).send(result);
         } else {
           res.status(404).send(err);
